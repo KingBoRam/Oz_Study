01254aad4c26418727c33fdadbfa06d53c9830c5
kimboram <bory2321@naver.com>
2024-04-08 18:33:04 +0900

    todolist에 localstorage저장 만듬..드래그앤드롭시 순서 바뀌도록까지 함 너무힘들었다진짜

--- challenge/js/ch02. 06~07/todolist.js
+++ challenge/js/ch02. 06~07/todolist.js
@@ -3,14 +3,24 @@ const btn = document.getElementById('add-btn');
 const ul = document.getElementById('todo-list');
 ul.style.paddingLeft = '0';
 ul.style.listStyleType = 'none';
-const list = [];
-console.log(list);
+const TODOS_Ls = 'toDos';
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_Ls, JSON.stringify(toDos));
+}
 
 btn.addEventListener('click', () => {
   if (input.value === '') {
     alert('할 일을 입력하세요.');
   } else {
-    list.push(input.value);
+    const newId = new Date().getTime();
+
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    li.draggable = 'true'; // 드래그 이벤트 발생시 부드럽게 작동함
+    li.id = newId;
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
 
@@ -19,20 +29,56 @@ btn.addEventListener('click', () => {
     deleteBtn.className = 'delete-btn';
     deleteBtn.onclick = function () {
       this.parentElement.remove();
+      const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id); //현재 이벤트가 발ㅅ생한 li.id와 다른 id를 가진 todo만 넣어라.
+      });
+      toDos = cleanToDos;
+      saveToDos();
     };
 
-    const li = document.createElement('li');
-
-    for (let i = 0; i < list.length; i++) {
-      li.textContent = input.value;
-      li.draggable = 'true';
-      li.prepend(checkbox);
-      li.append(deleteBtn);
-      ul.appendChild(li);
-    }
+    li.prepend(checkbox);
+    li.append(deleteBtn);
+    ul.appendChild(li);
+    const toDoObj = {
+      text: input.value,
+      id: newId,
+    };
+    toDos.push(toDoObj); // toDos에 toDoList 삽입
+    saveToDos(); // localStorage에 저장하는 함수
     input.value = '';
   }
 });
+
+const loadedToDos = localStorage.getItem(TODOS_Ls);
+if (loadedToDos !== null) {
+  let parsedToDos = JSON.parse(loadedToDos);
+  toDos = parsedToDos;
+  for (let i = 0; i < toDos.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = toDos[i].text;
+    li.id = toDos[i].id;
+    li.draggable = 'true';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'x';
+    deleteBtn.className = 'delete-btn';
+    deleteBtn.onclick = function () {
+      this.parentElement.remove();
+      const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id); //현재 이벤트가 발ㅅ생한 li.id와 다른 id를 가진 todo만 넣어라.
+      });
+      toDos = cleanToDos;
+      saveToDos();
+    };
+    li.prepend(checkbox);
+    li.append(deleteBtn);
+    ul.appendChild(li);
+  }
+}
+
 let picked = null;
 let pickedIndex = null;
 ul.addEventListener('dragstart', (e) => {
@@ -51,6 +97,20 @@ ul.addEventListener('drop', (e) => {
   } else {
     obj.before(picked);
   }
+  //드래그 했을 때 로컬스토리지 순서 변경
+  const updatedToDos = [];
+  let newToDos = ul.childNodes;
+  for (let i = 0; i < newToDos.length; i++) {
+    let num1 = 0;
+    let num2 = 0;
+    num1 = parseInt(newToDos[i].id);
+    for (let j = 0; j < toDos.length; j++) {
+      num2 = parseInt(toDos[j].id);
+      if (num1 === num2) {
+        updatedToDos.push(toDos[j]);
+      }
+    }
+  }
+  toDos = updatedToDos;
+  saveToDos();
 });
-// 로컬에추가하고 추가한거 새로고침해도 리스트에 보여주기까진했는데 새로고침하면 순서바뀌고
-// 새로고침후에 삭제하면 오류나서 못해먹겠어요 흑흑 오류의 늪이야
