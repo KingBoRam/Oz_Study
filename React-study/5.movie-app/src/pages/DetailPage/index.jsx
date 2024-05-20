fa7f6bf12b76915c866d91312983e3a033e45a57
김보람 <bory2321@naver.com>
2024-05-20 20:51:30 +0900

    react day9, detail page 스스로 만들기 과제

--- React-study/5.movie-app/src/pages/DetailPage/index.jsx
+++ React-study/5.movie-app/src/pages/DetailPage/index.jsx
@@ -2,6 +2,7 @@ import axios from '../../api/axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { imageBasePath } from '../../constant';
+import './DetailPage.css';
 
 const DetailPage = () => {
   const { movieId } = useParams();
@@ -15,12 +16,38 @@ const DetailPage = () => {
     }
     fetchData();
   }, [movieId]);
+  console.log(movie);
   if (!movie) {
     return null;
   }
+
+  const summeryText = (str, num) => {
+    return str?.length > num ? str.substring(0, num) + '...' : str;
+  };
+
   return (
-    <section>
-      <img src={`${imageBasePath}${movie.backdrop_path}`} alt="detail" />
+    <section className="search-container">
+      <img
+        className="movie-image"
+        src={`${imageBasePath}${movie.backdrop_path}`}
+        alt="detail"
+      />
+      <p className="search-title">{movie.original_title}</p>
+      <p className="search-genres">
+        {`장르 : `}
+        {movie.genres.map((item) => {
+          return (
+            <span className="search-genre" key={item.id}>
+              {item.name}
+            </span>
+          );
+        })}
+      </p>
+      <p className="search-average">{`평점 : ${movie.vote_average}`}</p>
+      <p className="search-summary">{`줄거리 : ${summeryText(
+        movie.overview,
+        200,
+      )}`}</p>
     </section>
   );
 };
