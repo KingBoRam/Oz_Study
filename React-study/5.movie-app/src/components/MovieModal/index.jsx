da4bd2b1239ed5e6831b6d80a7f863092d15b429
김보람 <bory2321@naver.com>
2024-05-20 17:42:58 +0900

    react day9, modal 외부 클릭시 꺼지기

--- React-study/5.movie-app/src/components/MovieModal/index.jsx
+++ React-study/5.movie-app/src/components/MovieModal/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { imageBasePath } from '../../constant';
 import './MovieModal.css';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 const MovieModal = ({
   backdrop_path,
@@ -12,10 +13,19 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
+  const ref = useRef(null);
+
+  console.log(ref);
+
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
+          {console.log(ref)}
           <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
