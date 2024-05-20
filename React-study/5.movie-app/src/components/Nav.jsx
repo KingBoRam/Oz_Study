6f80a61c3ddaae990c7c01e59af8958d78f34a8a
김보람 <bory2321@naver.com>
2024-05-20 15:50:50 +0900

    movie app row id 수정 + swipe로 수정하기 전 기록용

--- React-study/5.movie-app/src/components/Nav.jsx
+++ React-study/5.movie-app/src/components/Nav.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 const Nav = () => {
   const [show, setShow] = useState('false');
+
+  const [searchValue, setSearchValue] = useState('');
+
+  const navigate = useNavigate();
 
   const listener = () => {
     if (window.scrollY > 50) {
@@ -19,6 +24,11 @@ const Nav = () => {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <NavWrapper $show={show}>
       <Logo>
@@ -30,9 +40,44 @@ const Nav = () => {
           }}
         />
       </Logo>
+      <Input
+        type="text"
+        placeholder="영화를 검색해주세요."
+        className="nav__input"
+        value={searchValue}
+        onChange={handleChange}
+      ></Input>
+      <Login>로그인</Login>
     </NavWrapper>
   );
 };
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 5px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background.color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
 
 const NavWrapper = styled.nav`
   position: fixed;
--- React-study/5.movie-app/src/pages/SearchPage/index.jsx
+++ React-study/5.movie-app/src/pages/SearchPage/index.jsx
@@ -1,5 +1,73 @@
+import { useEffect, useState } from 'react';
+import axios from '../../api/axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './SearchPage.css';
+import { useDebounce } from '../../hooks/useDebounce';
+
 const SearchPage = () => {
-  return <div>index</div>;
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  let query = useQuery();
+  const searchTerm = query.get('q');
+  const deBouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  useEffect(() => {
+    if (deBouncedSearchTerm) {
+      fetchSearchMovie(deBouncedSearchTerm);
+    }
+  }, [deBouncedSearchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`,
+      );
+      console.log(response);
+      setSearchResults(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const navigate = useNavigate();
+
+  if (searchResults.length > 0) {
+    return (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== 'person') {
+            const movieImgUrl =
+              'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  onClick={() => {
+                    navigate(`/${movie.id}`);
+                  }}
+                  className="movie__column-poster"
+                >
+                  <img
+                    src={movieImgUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    );
+  } else {
+    <section className="no-results">
+      <div className="no-results__text">
+        <p>찾고자 하는 검색어 {searchTerm}에 맞는 영화가 없습니다.</p>
+      </div>
+    </section>;
+  }
 };
 
 export default SearchPage;
