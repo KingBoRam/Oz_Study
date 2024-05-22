b3c24b7f10dc3442015e39e22c06675b6c69d88d
김보람 <bory2321@naver.com>
2024-05-22 16:30:42 +0900

    react day11, React Context

---
+++ React-study/6.budget-app/components/ExpenseList.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { AppContext } from '../contexts/AppContext';
+import ExpenseItem from '../components/ExpenseItem';
+
+const ExpenseList = () => {
+  console.log('랜더링됨');
+  const { expenses } = useContext(AppContext);
+  console.log(expenses);
+
+  const [filteredExpenses, setFilteredExpenses] = useState(expenses || []);
+  console.log(filteredExpenses);
+
+  useEffect(() => {
+    setFilteredExpenses(expenses);
+  }, [expenses]);
+
+  const handleChange = (e) => {
+    const searchResults = expenses.filter((expense) => {
+      return expense.name.toLowerCase().includes(e.target.value);
+    });
+    setFilteredExpenses(searchResults);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="검색하기..."
+        onChange={handleChange}
+      ></input>
+      <ul className="list-group mt-3 mb-3">
+        {filteredExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              id={expense.id}
+              name={expense.name}
+              cost={expense.cost}
+            ></ExpenseItem>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default ExpenseList;
---
+++ React-study/6.budget-app/components/ViewBudget.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ViewBudget = () => {
+  return <div>ViewBudget</div>;
+};
+
+export default ViewBudget;
