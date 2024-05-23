e1c4418f252d4ea457008a56336e6973d51275ca
김보람 <bory2321@naver.com>
2024-05-23 15:18:31 +0900

    react day12, context로 state 업데이트하기

--- React-study/6.budget-app/components/Budget.jsx
+++ React-study/6.budget-app/components/Budget.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../contexts/AppContext';
+import EditBudget from '../components/EditBudget';
+import ViewBudget from '../components/ViewBudget';
 
 const Budget = () => {
-  return <div>Budget</div>;
+  const { budget, dispatch } = useContext(AppContext);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <div className="alert alert-primary p-3 d-flex align-items-center justify-content-between">
+      {isEditing ? (
+        <EditBudget
+          budget={budget}
+          setIsEditing={setIsEditing}
+          dispatch={dispatch}
+        ></EditBudget>
+      ) : (
+        <ViewBudget budget={budget} setIsEditing={setIsEditing}></ViewBudget>
+      )}
+    </div>
+  );
 };
 
 export default Budget;
--- React-study/6.budget-app/components/ViewBudget.jsx
+++ React-study/6.budget-app/components/ViewBudget.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { formatNumberToWon } from '../utils';
 
-const ViewBudget = () => {
-  return <div>ViewBudget</div>;
+const ViewBudget = ({ budget, setIsEditing }) => {
+  return (
+    <>
+      <span> 예산 : {formatNumberToWon(budget)}</span>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          setIsEditing(true);
+        }}
+      >
+        수정
+      </button>
+    </>
+  );
 };
 
 export default ViewBudget;
