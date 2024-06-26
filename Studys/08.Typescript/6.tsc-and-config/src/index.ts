5e8c4595f14a2cbeadd0c079bdcbb005f6c28051
김보람 <bory2321@naver.com>
2024-06-26 17:45:43 +0900

    ts day1, typscript의 설치와 설정, 그리고 타입과 타입주석

---
+++ Studys/08.Typescript/6.tsc-and-config/src/index.ts
@@ -0,0 +1,13 @@
+// console.log("Hello typescript");
+
+// const nums: number[] = [];
+
+// const a = 10 + nums[0];
+
+// const str = "a!";
+
+// str.endsWith("!");
+
+import {printName} from "./util";
+
+printName("type!");
